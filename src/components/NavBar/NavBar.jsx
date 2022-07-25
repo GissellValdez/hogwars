03605f8be9a5f7bde17721a8d0a74eaea15ba184
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-25 01:40:07 -0400

    implemented mern stack with token based authentication

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+// Don't forget the import
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service'
+
+export default function NavBar({user}, {setUser}) {
+  function handleLogOut(){
+    userService.logOut()
+    setUser(null)
+  }
+  return (
+    <nav>
+      <Link to="/link1">Link 1</Link>
+      &nbsp; | &nbsp;
+      <Link to="/link2">Link 2</Link>
+      &nbsp; | &nbsp;
+      <Link to="/" onClick={handleLogOut}>Logout</Link>
+      <br />
+      Welcome, {user.firstname} {user.lastname}
+    </nav>
+  );
+}
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,26 @@
+//import logo from '../logo.svg';
+import './App.css';
+import NavBar from "../../components/NavBar/NavBar.jsx";
+import AuthPage from "../AuthPage/AuthPage.jsx";
+import { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+  //const [userName, setUserName] = useState("");
+
+  return (
+    <main className="App">
+      {user ? 
+      <>
+        <NavBar user={user} setUser={setUser} />
+        <Routes>
+
+        </Routes>
+      </>
+       : 
+      <AuthPage setUser={setUser} />}
+    </main>
+  );
+}
