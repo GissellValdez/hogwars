4f0fd537f11b91f341e9c1877ed64be7c8531761
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-25 14:09:58 -0400

    updated app.js to include a books route for the HPBooksPage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import NavBar from "../../components/NavBar/NavBar.jsx";
 import AuthPage from "../AuthPage/AuthPage.jsx";
+import HPBooksPage from "../HPBooksPage/HPBooksPage.jsx";
 import { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
@@ -16,7 +17,7 @@ export default function App() {
       <>
         <NavBar user={user} setUser={setUser} />
         <Routes>
-
+          <Route path="/books" element={<HPBooksPage resource="books"/>} />
         </Routes>
       </>
        : 
