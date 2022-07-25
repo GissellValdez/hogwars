9354b490038a7cd379a5fb115ca137b0a3448c06
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-25 14:39:40 -0400

    updated navbar to includes links to characters and the HogWARS game

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,9 +11,13 @@ export default function NavBar({user}, {setUser}) {
     <nav>
       <Link to="/books">Books</Link>
       &nbsp; | &nbsp;
+      <Link to="/characters">Characters</Link>
+      &nbsp; | &nbsp;
       <Link to="/houses">Houses</Link>
       &nbsp; | &nbsp;
       <Link to="/species">Species</Link>
+      &nbsp; | &nbsp;
+      <Link to="/hogwars">HogWARS</Link>
       &nbsp; | &nbsp;
       <Link to="/" onClick={handleLogOut}>Logout</Link>
       <br />
