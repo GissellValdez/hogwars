0af62c0564b54f4218d655e5dc2a04a6221a86ed
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 19:16:21 -0400

    made the houses clickable in the house data tile component... detail is now rendering on screen when clicked

--- src/components/HPHouseDataTile/HPHouseDataTile.jsx
+++ src/components/HPHouseDataTile/HPHouseDataTile.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'
 export default function HPHouseDataTile(props){
     const image_url=props.resourceData.image_url
 
     return(
         <>
             <div>
+                <Link to={`/houses/${props.resourceData.id}`}>
                 <h4>{props.resourceData.name}</h4>
-                <img src={image_url}/>
+                <img src={image_url} alt="Not Available"/>
+                </Link>
             </div>
         </>
     )
