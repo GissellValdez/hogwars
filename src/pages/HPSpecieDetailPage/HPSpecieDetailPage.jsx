be968fa0716bd4d1c58963861fcd2f8b1787f58b
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 19:29:53 -0400

    added detail page for species

---
+++ src/pages/HPSpecieDetailPage/HPSpecieDetailPage.jsx
@@ -0,0 +1,35 @@
+import * as harryPotterService from "../../utilities/harry-potter-other-info/harry-potter-other-info-service"
+import { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+
+export default function HPSpecieDetailPage({resource}){
+    const [potterData, setPotterData] = useState([])
+    const { id } = useParams();
+    
+    async function getHarryPotterResourceData(resource){
+        resource+=id
+        const potterData = await harryPotterService.getHarryPotterResourceData(resource)
+        //const potterDataListJson = await potterDataList.Json()
+        setPotterData(potterData)
+        console.log(potterData)
+    }
+
+    useEffect(() => {
+        getHarryPotterResourceData(resource)
+    }, [])
+
+    return(
+        <>
+            <div>
+                <h1>Name: {potterData.name}</h1>
+                <h3>Main Location: {potterData.native}</h3>
+                <h3>Mortality: {potterData.mortality}</h3>
+                <h3>Eyes: {potterData.eyes}</h3>
+                <h3>Hair: {potterData.hair}</h3>
+                <h3>height: {potterData.height}</h3>
+                <h3>Distinctions: {potterData.distinctions.content}</h3>
+                <h3>Relates Species: {potterData.related_species.name}</h3>
+            </div>
+        </>
+    )
+}
