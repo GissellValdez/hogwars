cb93692c975269eb930dfe5db66e310fc5bbf20c
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 01:50:19 -0400

    fixed :id in app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "../../components/NavBar/NavBar.jsx";
 import AuthPage from "../AuthPage/AuthPage.jsx";
 import HPCharactersPage from "../HPCharactersPage/HPCharactersPage.jsx";
 import HPBooksPage from "../HPBooksPage/HPBooksPage.jsx";
+import HPBookDetailPage from "../HPBookDetailPage/HPBookDetailPage.jsx";
 import HPHousesPage from "../HPHousesPage/HPHousesPage.jsx";
 import HPSpeciesPage from "../HPSpeciesPage/HPSpeciesPage.jsx";
 import { useState } from "react";
@@ -22,6 +23,7 @@ export default function App() {
         <Routes>
           <Route path="/characters" element={<HPCharactersPage />} />
           <Route path="/books" element={<HPBooksPage resource="books"/>} />
+          <Route path="/books/:id" element={<HPBookDetailPage resource="books"/>} />
           <Route path="/houses" element={<HPHousesPage resource="houses"/>} />
           <Route path="/species" element={<HPSpeciesPage resource="species"/>} />
         </Routes>
