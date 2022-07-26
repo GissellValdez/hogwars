aa24bbd6330eb006cccbafd116fabdddd5754d8b
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 01:36:04 -0400

    added details page for books, next step is to make the books clickable

---
+++ src/pages/HPBookDetailPage/HPBookDetailPage.jsx
@@ -0,0 +1,37 @@
+import * as harryPotterService from "../../../utilities/harry-potter-other-info/harry-potter-other-info-service"
+import { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+
+export default function HPBookDetailPage({resource}){
+    const [potterData, setPotterData] = useState([])
+    const { id } = useParams();
+    
+    async function getHarryPotterResourceData(resource){
+        resource+=id
+        const potterData = await harryPotterService.getHarryPotterResourceData(resource)
+        //const potterDataListJson = await potterDataList.Json()
+        setPotterData(potterData)
+        console.log(potterData)
+    }
+
+    useEffect(() => {
+        getHarryPotterResourceData(resource)
+    }, [])
+
+    return(
+        <>
+            <div>
+                <img src={potterData.image_url}/>
+            </div>
+            <div>
+                <h1>Book #: {potterData.id}</h1>
+                <h3>Title: {potterData.title}</h3>
+                <h3>Page Count: {potterData.pages}</h3>
+                <h3>Release Date: {potterData.release_date}</h3>
+                <h3>Publisher: {potterData.publisher}</h3>
+                <h3>Previous Book: {potterData.preceded_by}</h3>
+                <h3>Next Book: {potterData.followed_by}</h3>
+            </div>
+        </>
+    )
+}
