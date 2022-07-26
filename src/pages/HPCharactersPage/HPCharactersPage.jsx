d212f2ee3e9322f3ec4a2131f07431f719c3f7a1
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-25 21:25:19 -0400

    added a page for characters

---
+++ src/pages/HPCharactersPage/HPCharactersPage.jsx
@@ -0,0 +1,9 @@
+import HPCharacterSearchBar from "../../components/HPCharacterSearchBar/HPCharacterSearchBar"
+
+export default function HPCharactersPage(){
+    return(
+        <>
+            < HPCharacterSearchBar />
+        </>
+    )
+}
