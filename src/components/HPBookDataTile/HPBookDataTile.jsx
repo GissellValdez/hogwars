bc1d4e45adbfda7f40ff181d8b5d6a28608c1816
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 01:41:28 -0400

    made the books clickable in the BookDataTile component, next step is to add the route in app.js

--- src/components/HPBookDataTile/HPBookDataTile.jsx
+++ src/components/HPBookDataTile/HPBookDataTile.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'
 export default function HPBookDataTile(props){
     const image_url=props.resourceData.image_url
 
     return(
         <>
             <div>
-                <h4>{props.resourceData.title}</h4>
-                <img src={image_url}/>
+                <Link to={`/books/${props.resourceData.id}`}>
+                    <h4>{props.resourceData.title}</h4>
+                    <img src={image_url} alt="Not Available"/>
+                </Link>
             </div>
         </>
     )
