e3f357f299fb55e546f9c09fa9d9f1107ed502de
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 17:48:40 -0400

    updated player avatar component to display test text

--- src/components/HogWARSPlayerAvatar/HogWARSPlayerAvatar.jsx
+++ src/components/HogWARSPlayerAvatar/HogWARSPlayerAvatar.jsx
@@ -1,1 +1,11 @@
 // component for the player avatar on the game board
+
+import { Component } from "react";
+
+export default class HogWARSPlayerAvatar extends Component {
+    render(){
+        return(
+            <h2>PlayableCharacter</h2>
+        )
+    }
+}
