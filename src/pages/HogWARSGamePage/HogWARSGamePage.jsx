f85310e5246f4ad17e0264a159dbce54ed25df21
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 16:55:42 -0400

    added a state variable with temporary game data for testing purposes

--- src/pages/HogWARSGamePage/HogWARSGamePage.jsx
+++ src/pages/HogWARSGamePage/HogWARSGamePage.jsx
@@ -1,6 +1,26 @@
 // page file for the HogWARS game
 
 export default function HogWARSGamePage(){
+    const state = {
+        player1: {
+            name: 'Harry Potter',
+            house: 'Gryffindor',
+            playerImg: '',
+            opponentImg: '',
+            hp: 500
+        },
+        player2: {
+            name: 'Lord Voldemort',
+            house: 'Slytherin',
+            playerImg: '',
+            opponentImg: '',
+            hp: 500
+        },
+        playerFaint: false,
+        opponentFaint: false,
+        gameOver: false
+    }
+
     return(
         <h1>Hi</h1>
     )
