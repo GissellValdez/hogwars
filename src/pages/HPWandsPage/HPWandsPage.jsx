1d156838c7f2c4824c2bb52e9c09a9fbe746c68d
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-26 22:35:25 -0400

    added a new page for wands

---
+++ src/pages/HPWandsPage/HPWandsPage.jsx
@@ -0,0 +1,29 @@
+import * as harryPotterService from "../../utilities/harry-potter-other-info/harry-potter-other-info-service"
+import { useEffect, useState } from 'react'
+import HPWandDataTile from "../../components/HPWandDataTile/HPWandDataTile"
+
+export default function HPWandsPage({resource}){
+    const [potterDataList, setPotterDataList] = useState([])
+
+    async function getHarryPotterResourceData(resource){
+        const potterDataList = await harryPotterService.getHarryPotterResourceData(resource)
+        //const potterDataListJson = await potterDataList.Json()
+     setPotterDataList(potterDataList)
+        console.log(potterDataList)
+    }
+
+    useEffect(() => {
+        getHarryPotterResourceData(resource)
+    }, [])
+
+    return(
+        <>
+            <div>
+                {potterDataList?.map((d, idx) =>
+                (
+                    <HPWandDataTile resourceData={d} key={idx} />
+                ))}
+            </div>
+        </>
+    )
+}
