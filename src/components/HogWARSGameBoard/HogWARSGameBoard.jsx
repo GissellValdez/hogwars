cd419d9af3887311a181718a098a8ee93eb13342
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 17:25:35 -0400

    moved code from hogwars page to the hogwars game baord component

--- src/components/HogWARSGameBoard/HogWARSGameBoard.jsx
+++ src/components/HogWARSGameBoard/HogWARSGameBoard.jsx
@@ -1,1 +1,30 @@
 // game board component rendered by the HogWARSGamePage
+
+import { Component } from 'react'
+export default class HogWARSGameBoard extends Component {
+    state = {
+        player1: {
+            name: 'Harry Potter',
+            house: 'Gryffindor',
+            playerImg: '',
+            opponentImg: '',
+            hp: 500
+        },
+        player2: {
+            name: 'Lord Voldemort',
+            house: 'Slytherin',
+            playerImg: '',
+            opponentImg: '',
+            hp: 500
+        },
+        playerFaint: false,
+        opponentFaint: false,
+        gameOver: false
+    }
+
+    render(){
+        return(
+        <h1>Hi</h1>
+        )
+    }
+}
