c631ad77c54d12383ebdb4c9820082025dd2e6eb
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 01:00:12 -0400

    added playable characters schema to model

--- models/playable-character.js
+++ models/playable-character.js
@@ -3,3 +3,10 @@
 // models/user.js
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const playableCharacterSchema = new Schema({
+	name: { type: String, required: true },
+	house: { type: String, required: true },
+	playerImg: { type: String, required: true },
+	opponentImg: { type: String, required: true },
+})
