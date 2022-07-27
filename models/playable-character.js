043941128bbf1926db9d843a886e8f2171f7868f
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 16:57:49 -0400

    updated model to include playable character's hp

--- models/playable-character.js
+++ models/playable-character.js
@@ -8,6 +8,7 @@ const playableCharacterSchema = new Schema({
 	house: { type: String, required: true },
 	playerImg: { type: String, required: true },
 	opponentImg: { type: String, required: true },
+	hp: Number,
     spells: [{ type: Schema.Types.ObjectId, ref: 'Spell' }]
 })
 
