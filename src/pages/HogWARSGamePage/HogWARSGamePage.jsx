28ad43bcd4b9e9de8fc76220455701d61f83ecf4
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 17:30:42 -0400

    hogwars game page is now rendering the the game board component

--- src/pages/HogWARSGamePage/HogWARSGamePage.jsx
+++ src/pages/HogWARSGamePage/HogWARSGamePage.jsx
@@ -1,1 +1,8 @@
 // page file for the HogWARS game
+import HogWARSGameBoard from "../../components/HogWARSGameBoard/HogWARSGameBoard"
+
+export default function HogWARSGame (){
+    return(
+        <HogWARSGameBoard />
+    )
+}
