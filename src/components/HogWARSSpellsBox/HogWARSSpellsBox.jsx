a9e0b772f276c06166c302c44c9830a8b7601c61
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 22:26:03 -0400

    renamed spell box component to spells box

--- src/components/HogWARSSpellsBox/HogWARSSpellsBox.jsx
+++ src/components/HogWARSSpellsBox/HogWARSSpellsBox.jsx
@@ -1,7 +1,7 @@
 // HogWARSSpellBox - component that will display the playable character's spells for player to click and attach the opponent
 
 import { Component } from 'react'
-export default class HogWARSSpellBox extends Component {
+export default class HogWARSSpellsBox extends Component {
     render(){
         return(
             <h5>Spell Box</h5>
