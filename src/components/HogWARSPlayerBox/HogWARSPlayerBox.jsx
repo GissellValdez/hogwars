5c0ce50ecc9ef02af82f23d978aabeffa39cbc14
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 21:14:06 -0400

    playerbox component rendering avatar and healthbox components

--- src/components/HogWARSPlayerBox/HogWARSPlayerBox.jsx
+++ src/components/HogWARSPlayerBox/HogWARSPlayerBox.jsx
@@ -1,1 +1,17 @@
 // HogWARSPlayerBox component that will have player components (avatar and healthbox)
+
+import { Component } from 'react'
+import HogWARSPlayerAvatar from '../HogWARSPlayerAvatar/HogWARSPlayerAvatar'
+import HogWARSPlayerHealthBox from '../HogWARSPlayerHealthBox/HogWARSPlayerHealthBox'
+
+export default class HogWARSPlayerBox extends Component{
+    render(){
+        return(
+            <>
+                <h1>Game Board</h1>
+                <HogWARSPlayerAvatar playerAvatar={this.props.player} />
+                <HogWARSPlayerHealthBox playerHealth={this.props.player} />
+            </>
+        )
+    }
+}
