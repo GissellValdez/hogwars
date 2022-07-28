69bd573ce9230976a4b30f1f5a11a0ab283aa9d9
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 22:38:36 -0400

    updated spellbox to loop through all spells in prop and render a spell component for it

--- src/components/HogWARSSpellsBox/HogWARSSpellsBox.jsx
+++ src/components/HogWARSSpellsBox/HogWARSSpellsBox.jsx
@@ -1,10 +1,25 @@
 // HogWARSSpellBox - component that will display the playable character's spells for player to click and attach the opponent
 
 import { Component } from 'react'
+import Spell from '../HogWARSSpell/HogWARSSpell'
+
 export default class HogWARSSpellsBox extends Component {
     render(){
         return(
-            <h5>Spell Box</h5>
+            <>
+                <h5>Spell Box</h5>
+                {
+                    Object.keys(this.props.spells).map((key, index) => {
+                        return (
+                            <Spell
+                                key={key}
+                                index={index}
+                                details={this.props.spells[key]}
+                            />
+                        )
+                    })
+                }
+            </>
         )
     }
 }
