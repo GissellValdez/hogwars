4e08c23ddc0aec279fe8494dfd0415629e0607b1
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 22:22:17 -0400

    added a test h5 element to test rendering to screen

--- src/components/HogWARSSpellBox/HogWARSSpellBox.jsx
+++ src/components/HogWARSSpellBox/HogWARSSpellBox.jsx
@@ -1,1 +1,10 @@
 // HogWARSSpellBox - component that will display the playable character's spells for player to click and attach the opponent
+
+import { Component } from 'react'
+export default class HogWARSSpellBox extends Component {
+    render(){
+        return(
+            <h5>Spell Box</h5>
+        )
+    }
+}
