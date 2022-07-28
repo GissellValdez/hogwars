54dd6677c56b9cc9bb276a66806d17b27fb44415
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-27 21:22:30 -0400

    added alt to img tag in avatar component

--- src/components/HogWARSPlayerAvatar/HogWARSPlayerAvatar.jsx
+++ src/components/HogWARSPlayerAvatar/HogWARSPlayerAvatar.jsx
@@ -7,7 +7,7 @@ export default class HogWARSPlayerAvatar extends Component {
         return(
             <>
                 <h2>PlayableCharacter</h2>
-                <img src={this.props.playerAvatar.playerImg}/>
+                <img src={this.props.playerAvatar.playerImg} alt="Not Available" />
             </>
         )
     }
