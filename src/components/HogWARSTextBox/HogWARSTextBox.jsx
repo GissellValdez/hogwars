9d5d498e8334e54a42cfd415bc48d0d48aba9d57
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-28 19:03:12 -0400

    updated the textbox component to render a p element that will display the value of the passed in prop

--- src/components/HogWARSTextBox/HogWARSTextBox.jsx
+++ src/components/HogWARSTextBox/HogWARSTextBox.jsx
@@ -1,1 +1,13 @@
 // HogWARSTextBox - this component will be displaying text throughout the game.
+
+import { Component } from 'react'
+
+export default class HogWARSTextBox extends Component {
+    render(){
+        return(
+            <p>
+                {this.props.gameText}
+            </p>
+        )
+    }
+}
