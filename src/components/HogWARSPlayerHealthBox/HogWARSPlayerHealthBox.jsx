421a4e0f1f8dd9fa2ef1a63ec429fc523881b61c
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-28 22:14:44 -0400

    updated the hp to show currentHP/PlayerHP to show how close to 0 the hp is

--- src/components/HogWARSPlayerHealthBox/HogWARSPlayerHealthBox.jsx
+++ src/components/HogWARSPlayerHealthBox/HogWARSPlayerHealthBox.jsx
@@ -4,11 +4,12 @@ import { Component } from "react";
 
 export default class HogWARSPlayerHealthBox extends Component {
     render(){
+                let currentHP = (this.props.playerHealth.hp);
         return(
             <>
                 <div className='playerHealthBox'>
                     <h2>{this.props.playerHealth.name}</h2>
-                    <h3>HP: {this.props.playerHealth.hp}</h3>
+                    <h3>HP: {currentHP}/{this.props.playerHealth.hp}</h3>
                 </div>
             </>
         )
