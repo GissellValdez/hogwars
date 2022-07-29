191f8cfa2d042f9e0183553ed7c5bc2a0c0f781f
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-28 22:45:59 -0400

    encased healthbox elements in adiv and gave it className opponentHealthBox for styling, also added current hp / opp health to view current health vs total health

--- src/components/HogWARSOpponentHealthBox/HogWARSOpponentHealthBox.jsx
+++ src/components/HogWARSOpponentHealthBox/HogWARSOpponentHealthBox.jsx
@@ -4,10 +4,13 @@ import { Component } from "react";
 
 export default class HogWARSOpponentHealthBox extends Component {
     render(){
+        let currentOpponentHP = (this.props.opponentHealth.hp)
         return(
             <>
-                <h2>{this.props.opponentHealth.name}</h2>
-                <h3>HP: {this.props.opponentHealth.hp}</h3>
+                <div className="opponentHealthBox">
+                    <h2>{this.props.opponentHealth.name}</h2>
+                    <h3>HP: {currentOpponentHP}/{this.props.opponentHealth.hp}</h3>
+                </div>
             </>
         )
     }
