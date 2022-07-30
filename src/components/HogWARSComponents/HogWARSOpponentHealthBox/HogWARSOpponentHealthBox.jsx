abf415c20f459b24ff080782d37d68fa705a6127
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-30 17:39:03 -0400

    updated to use the new currentHP property in the state, which was added in the previous commit

--- src/components/HogWARSComponents/HogWARSOpponentHealthBox/HogWARSOpponentHealthBox.jsx
+++ src/components/HogWARSComponents/HogWARSOpponentHealthBox/HogWARSOpponentHealthBox.jsx
@@ -4,12 +4,11 @@ import { Component } from "react";
 
 export default class HogWARSOpponentHealthBox extends Component {
     render(){
-        let currentOpponentHP = (this.props.opponentHealth.hp)
         return(
             <>
                 <div className="opponentHealthBox">
                     <h2>{this.props.opponentHealth.name}</h2>
-                    <h3>HP: {currentOpponentHP}/{this.props.opponentHealth.hp}</h3>
+                    <h3>HP: {this.props.opponentHealth.currentHP}/{this.props.opponentHealth.maxHP}</h3>
                 </div>
             </>
         )
