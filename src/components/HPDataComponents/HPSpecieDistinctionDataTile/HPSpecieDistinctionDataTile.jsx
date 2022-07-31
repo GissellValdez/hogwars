dd3a4acddf84d6a803b15851dfd770725504cf0c
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-31 19:42:08 -0400

    created a new component for the species distinctions, needed because i had to map through them

---
+++ src/components/HPDataComponents/HPSpecieDistinctionDataTile/HPSpecieDistinctionDataTile.jsx
@@ -0,0 +1,10 @@
+export default function HPSpecieDistinctionDataTile(props){
+
+    return(
+        <>
+            <div>
+                <li>{props.specieDistinction.content}</li>
+            </div>
+        </>
+    )
+}
