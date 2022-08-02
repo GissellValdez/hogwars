5cf0e95899bf2e4d8f2fb7f18b4312b513fd0aa4
“GissellValdez” <Gissellvaldez@gmail.com>
2022-08-02 09:50:19 -0400

    updated the auth page with intro image and headers for the sign up and login components

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,9 +4,13 @@ import LoginForm from "../../components/LoginForm/LoginForm.jsx";
 export default function AuthPage(props) {
   return (
     <main>
-      <h1>AuthPage</h1>
+      <h1>Sign Up!</h1>
       <SignUpForm setUser={props.setUser}/>
+      <h1>Log In!</h1>
       <LoginForm setUser={props.setUser}/>
+      <div className="intro-img">
+        <img src="https://i.ibb.co/K7FRrjF/Hog-WARSIntro.jpg" alt="Hog-WARSIntro" border="0" />
+      </div>
     </main>
   );
 }
