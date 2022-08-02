b35e1f2ff2e63de89a6b2837242b0dfd3ad58d04
“GissellValdez” <Gissellvaldez@gmail.com>
2022-08-02 10:29:27 -0400

    fixed import of user service in my account page

--- src/pages/MyAccount/MyAccount.jsx
+++ src/pages/MyAccount/MyAccount.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import  { Link } from 'react-router-dom'
-import * as userService from "../../utilities/users/users-services"
+import * as userService from "../../utilities/users-service"
 
 
 export default function MyAccount(props) {
